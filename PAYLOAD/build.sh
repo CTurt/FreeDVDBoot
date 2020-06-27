a40355aa630664cd2c6218bdd06cc8df0f05c48d
CTurt <ecturt@gmail.com>
2020-06-27 15:44:39 +0100

    Recompile with yesterday's fix

--- PAYLOAD/build.sh
+++ PAYLOAD/build.sh
@@ -4,8 +4,13 @@ ee-gcc -Ttext=0x01FFF800 payload.c -o payload.elf -nostartfiles -nostdlib -ffree
 ee-objcopy -O binary payload.elf payload.bin -Wl,-z,max-page-size=0x1
 
 ENTRY=`ee-objdump -t payload.elf | grep " _start"`
-ENTRY=0x`grep -o "^\S*" <<< $ENTRY`
+echo $ENTRY
 
+# Doesn't seem to work on MinGW toolchain, so set manually if you're using that:
+ENTRY=0x`grep -o "^\S*" <<< $ENTRY`
+#ENTRY=0x01fff9a8
+
+echo $ENTRY
 echo "Building crt0"
 
 ee-gcc -Ttext=0x01500014 -DENTRY=$ENTRY crt0.S -o fullpayload.elf -nostartfiles -nostdlib -ffreestanding -Wl,-z,max-page-size=0x1
