478787c9acc95fae6ab18538b7c935539c4b8617
CTurt <ecturt@gmail.com>
2020-07-19 15:23:20 +0100

    Move payloads to same place

--- PAYLOADS/build.sh
+++ PAYLOADS/build.sh
@@ -12,6 +12,14 @@ ENTRY=0x01fff99c
 
 echo $ENTRY
 
+echo "Building crt0 (3.03)"
+ee-gcc -Ttext=0x015FFF34 -DENTRY=$ENTRY -DGETBUFFERINTERNAL=0x262360 crt0.S -o crt0_3.03.elf -nostartfiles -nostdlib -ffreestanding -Wl,-z,max-page-size=0x1
+ee-objcopy -O binary crt0_3.03.elf crt0_3.03.bin -Wl,-z,max-page-size=0x1
+
+echo "Building crt0 (3.04M)"
+ee-gcc -Ttext=0x01800180 -DENTRY=$ENTRY -DGETBUFFERINTERNAL=0x261548 crt0.S -o crt0_3.04M.elf -nostartfiles -nostdlib -ffreestanding -Wl,-z,max-page-size=0x1
+ee-objcopy -O binary crt0_3.04M.elf crt0_3.04M.bin -Wl,-z,max-page-size=0x1
+
 echo "Building jump for 3.04J"
 ee-gcc -Ttext=0x012811E4 -DJUMP=0x01281340 jump.S -o jump.elf -nostartfiles -nostdlib -ffreestanding -Wl,-z,max-page-size=0x1
 ee-objcopy -O binary jump.elf jump.bin -Wl,-z,max-page-size=0x1
@@ -29,8 +37,14 @@ ee-gcc -Ttext=0x01500014 -DENTRY=$ENTRY -DGETBUFFERINTERNAL=0x2952f0 crt0.S -o c
 ee-objcopy -O binary crt0_3.11.elf crt0_3.11.bin -Wl,-z,max-page-size=0x1
 
 echo "Done."
+
+echo "For the All Slims image:"
+echo "Insert crt0_3.03.bin into VIDEO_TS.IFO at offset 0x0e8c"
 echo "Insert jump.bin into VIDEO_TS.IFO at offset 0x2724"
 echo "Insert crt0_3.04J.bin into VIDEO_TS.IFO at offset 0x2880"
 echo "Insert crt0_3.10.bin into VIDEO_TS.IFO at offset 0x2bb4"
 echo "Insert crt0_3.11.bin into VIDEO_TS.IFO at offset 0x2954"
 echo "Insert payload.bin into VIDEO_TS.IFO at offset 0x3000"
+
+echo "For 3.04M only image:"
+echo "Insert fullpayload.bin at 0x2d00, and payload.bin at 0x3000"
--- PAYLOADS/payload.c
+++ PAYLOADS/payload.c
@@ -6,15 +6,15 @@
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
-#define pointToIFO ((void (*)(unsigned int index, unsigned int lba, unsigned int offset))0x23dfc8)
-#define getDiscData ((void (*)(unsigned int s, void *d))0x23e138)
+void (*pointToIFO)(unsigned int index, unsigned int lba, unsigned int offset);
+void (*getDiscData)(unsigned int s, void *d);
 
-#define SifIopReset ((int (*)(char *, int))0x291358)
-#define SifIopSync ((int (*)(void))0x2914d8)
-#define SifInitRpc ((void (*)(int))0x208260)
-#define SifExitRpc ((void (*)(void))0x208400)
+int (*getBufferInternal)(void *filename, int type, int currentSector, void *dest, unsigned int sectorsRemaining, int curReadPos);
 
-#define getBufferInternal ((int (*)(void *filename, int type, int currentSector, void *dest, unsigned int sectorsRemaining, int curReadPos))0x261548)
+int (*SifIopReset)(char *, int);
+int (*SifIopSync)(void);
+void (*SifInitRpc)(int);
+void (*SifExitRpc)(void);
 
 #define ELF_PT_LOAD 1
 
@@ -55,13 +55,14 @@ __attribute__((noreturn)) void ExecPS2(void *entry, void *gp, int argc, char **a
 	//__builtin_unreachable();
 }
 
-void *memcpy_(void *dest, void *src, size_t n) { 
+static void *memcpy_(void *dest, void *src, size_t n) { 
    int i;
    for(i = 0; i < n; i++) ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
    return dest;
 }
 
-void *memset(void *dest, int c, size_t n) {
+// Todo: maybe cache last sector to save 1 or 2 reads
+static void *memset(void *dest, int c, size_t n) {
 	int i;
 	for(i = 0; i < n; i++) ((unsigned char *)dest)[i] = c;
 	return dest;
@@ -91,10 +92,57 @@ static void readData(void *dest, unsigned int offset, size_t n) {
 }
 
 __attribute__((noreturn)) void _start(void) {
-	//Exit(0);
-	//asm volatile("la $v1, 0x04; la $a0, 0; syscall 0x04");
-
 	int i;
+
+	// Identify version based on jump target location
+	if((*(void **)0x928D24) == (void *)0x15ea540) {
+		// 3.03
+		pointToIFO = (void *)0x2432c8;
+		getDiscData = (void *)0x243438;
+
+		getBufferInternal = (void *)0x262360;
+
+		SifIopReset = (void *)0x291fb8;
+		SifIopSync = (void *)0x292138;
+		SifInitRpc = (void *)0x2082a0;
+		SifExitRpc = (void *)0x208440;
+	}
+	else if((*(void **)0x6D9C3C) == (void *)0x126b7e0) {
+		// 3.04J
+		pointToIFO = (void *)0x23dfe0;
+		getDiscData = (void *)0x23e150;
+
+		getBufferInternal = (void *)0x261560;
+
+		SifIopReset = (void *)0x84fe0;
+		SifIopSync = (void *)0x85110;
+		SifInitRpc = (void *)0x84180;
+		SifExitRpc = (void *)0x84310;
+	}
+	else if((*(void **)0x5f1f38) == (void *)0x1500014) {
+		// 3.10
+		pointToIFO = (void *)0x25c880;
+		getDiscData = (void *)0x25c9f0;
+		
+		getBufferInternal = (void *)0x002986a0;
+
+		SifIopReset = (void *)0x84fe0;
+		SifIopSync = (void *)0x85110;
+		SifInitRpc = (void *)0x84180;
+		SifExitRpc = (void *)0x84310;
+	}
+	else if((*(void **)0x3EA438) == (void *)0x1500014) {
+		// 3.11
+		pointToIFO = (void *)0x258a28;
+		getDiscData = (void *) 0x258b98;
+
+		getBufferInternal = (void *)0x2952f0;
+
+		SifIopReset = (void *)0x20e7d8;
+		SifIopSync = (void *)0x20e958;
+		SifInitRpc = (void *)0x208d80;
+		SifExitRpc = (void *)0x208f20;
+	}
 
 	#ifdef LOAD_FROM_VTS_02_0_IFO
 		// point to VTS_02_0.IFO
@@ -137,5 +185,6 @@ __attribute__((noreturn)) void _start(void) {
 	SifInitRpc(0);
 	SifExitRpc();
 
-	ExecPS2((void *)eh.entry, 0, 0, 0);
+	char *argv[] = { "cdrom0:\\VIDEO_TS\\VTS_02_0.IFO" };
+	ExecPS2((void *)eh.entry, 0, 1, &argv);
 }
